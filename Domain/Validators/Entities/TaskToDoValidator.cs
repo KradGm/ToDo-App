17daf1ca50557625b192c69e86e602e92efcd7cd
Enzo <enzo.loliveira@hotmail.com>
2024-03-11 09:20:05 -0300

    id validator instead of name validator

--- Domain/Validators/Entities/TaskToDoValidator.cs
+++ Domain/Validators/Entities/TaskToDoValidator.cs
@@ -15,14 +15,11 @@ public class TaskToDoValidator : AbstractValidator<TaskToDo>
         RuleFor(task => task.TaskName)
             .NotNull().WithMessage("É necessario ter um nome")
             .MinimumLength(1).WithMessage("O nome deve ter ao menos 1 caractere.")
-            .MaximumLength(20).WithMessage("Esse nome passa de  20 caracteres")
-            .Must(BeUniqueName)
-            .WithMessage("Este nome já está em uso por outra tarefa.");
+            .MaximumLength(20).WithMessage("Esse nome passa de  20 caracteres");
             
         RuleFor(task => task.TaskName)
             .Must(name => !name.StartsWith(" ")).WithMessage("As tarefas não devem começar com espaço")
             .Matches("^[a-zA-Z0-9 ]*$").WithMessage("O nome da tarefa só pode conter letras, números e espaços");
-
 
         RuleFor(task => task.Status)
             .IsInEnum()
@@ -32,12 +29,9 @@ public class TaskToDoValidator : AbstractValidator<TaskToDo>
             .MaximumLength(255)
             .WithMessage("A descrição não pode ter mais de 255 caracteres");
     }
-    //Still working on this uniqueValidation
-    private bool BeUniqueName(TaskToDo task, string name)
-    {
-    var existingTask = _context.Tasks.Where(tsk => tsk.TaskName.ToLower() == name.ToLower()&& tsk.Id == task.Id).SingleOrDefault();
+    private bool isUnique(TaskToDo task, long id){
+        return !_context.Tasks.Any(t=>t.Id == id && t!=task);
+    }
 
-    return existingTask==null;
-    }   
 }
 
